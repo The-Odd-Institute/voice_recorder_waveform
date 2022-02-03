2d938a408e0fb4ecbf41105f822c8e2a2187f3c3
Ivan Trj <ivantrajanovski@gmail.com>
2022-02-03 19:55:55 +0100

    Initial Commit

---
+++ AudioRecorder/AudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderApp.swift
+//  AudioRecorder
+//
+//  Created by Ivan Trajanovski on 2022. 02. 03..
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
