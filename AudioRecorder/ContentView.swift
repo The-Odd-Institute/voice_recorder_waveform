5c722bbce30b8bb27155ea6298f6b04f6aee111a
Ivan Trj <ivantrajanovski@gmail.com>
2022-02-08 19:34:26 +0100

    add pause and stop buttons

--- AudioRecorder/ContentView.swift
+++ AudioRecorder/ContentView.swift
@@ -10,17 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            HStack {
-                Button(action: {print("Start recording")}) {
-                    Image(systemName: "waveform.circle.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 80, height: 80)
-                        .foregroundColor(.red)
-                        .padding(10)
+            VStack {
+                Spacer()
+                HStack(spacing: 16) {
+                    Button(action: {print("Stop")}) {
+                        Image(systemName: "stop.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 60, height: 60)
+                            .foregroundColor(.red)
+                            .padding(10)
+                    }
+                    Button(action: {print("Start recording")}) {
+                        Image(systemName: "waveform.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 80, height: 80)
+                            .foregroundColor(.red)
+                            .padding(10)
+                    }
+                    Button(action: {print("Pause")}) {
+                        Image(systemName: "pause.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 60, height: 60)
+                            .foregroundColor(.red)
+                            .padding(10)
+                    }
                 }
+                .navigationBarTitle("Voice Recorder")
             }
-            .navigationBarTitle("Voice Recorder")
         }
     }
 }
