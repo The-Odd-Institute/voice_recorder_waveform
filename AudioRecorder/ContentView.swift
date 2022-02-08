cfc455448ddc4692142c7c86bb3cdc8e353ab173
Ivan Trj <ivantrajanovski@gmail.com>
2022-02-08 17:20:24 +0100

    add record button

--- AudioRecorder/ContentView.swift
+++ AudioRecorder/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello!")
-            .padding()
+        NavigationView {
+            HStack {
+                Button(action: {print("Start recording")}) {
+                    Image(systemName: "waveform.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.red)
+                        .padding(10)
+                }
+            }
+            .navigationBarTitle("Voice Recorder")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
